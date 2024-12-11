e5aac4bccb04b89e3937f8b6676ba58e3c0033b5
Vinithgouda <vinuthm27@gmail.com>
2024-12-11 21:19:25 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,40 @@
+#creating a dict
+my_dict={'name':'raam','place':'banglore','age':23}
+#adding  to a dictionary
+my_dict['gender']='male'
+#removing 
+del my_dict['place']
+#modifying 
+my_dict['age']=45
+
+print("updated dictionary:",my_dict)
+
+
+
+
+#creating a set  
+my_set={2,4,6,8,10}
+#adding  
+my_set.add(7)
+#removing 
+my_set.remove(4) 
+#modifying 
+my_set.discard(2)
+my_set.add(3) 
+print("updated set:", my_set) 
+
+
+
+
+#creating a list
+my_list=[2,4,6,8,10]
+#adding  
+my_list.append(7)
+#removing 
+my_list.remove(4) 
+#modifying 
+my_list[0]=1
+print("updated list:", my_list) 
+
+
+
